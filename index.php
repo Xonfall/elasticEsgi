80a2bca3a593f95484bfa96e954ea64f80605936
Kevin <kev_kevin@hotmail.fr>
2019-03-26 18:22:47 +0100

    add correct format json

--- index.php
+++ index.php
@@ -14,15 +14,24 @@ foreach (file('source.csv') as $line) {
 
 	//echo $row[2].'<br>';
 
+	$row[0] = str_replace('"', "'", $row[0]);
+	$row[1] = str_replace('"', "'", $row[1]);
+	$row[2] = str_replace('"', "'", $row[2]);
+	$row[3] = str_replace('"', "'", $row[3]);
+	$row[4] = str_replace('"', "'", $row[4]);
+	$row[5] = str_replace('"', "'", $row[5]);
+	$row[6] = str_replace('"', "'", $row[6]);
+	$row[7] = str_replace('"', "'", $row[7]);
+
 	$tab = array(
-		"title" => $row[0],
-		"seo_title" => $row[1],
-		"url" => $row[2],
-		"author" => array('name' => $row[3]),
-		"date" => $row[4],
-		"category" => $row[5],
-		"locales" => $row[6],
-		"content" => $row[7]
+		"title" => htmlentities($row[0]),
+		"seo_title" => htmlentities($row[1]),
+		"url" => htmlentities($row[2]),
+		"author" => array('name' => htmlentities($row[3])),
+		"date" => htmlentities($row[4]),
+		"category" => htmlentities($row[5]),
+		"locales" => htmlentities($row[6]),
+		"content" => htmlentities($row[7])
 	);
 
 
