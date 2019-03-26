42edb08544fed5149ab75e30318a15534b59c307
Kevin <kev_kevin@hotmail.fr>
2019-03-26 17:57:27 +0100

    add json file

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+//header("Content-Type: application/json");
+
+$i = 1;
+
+$tab = array();
+
+foreach (file('source.csv') as $line) {
+	$row = explode(';', $line);
+
+	echo '{"index": {"_id": '.$i.'}}';
+	echo '<br>';
+
+	//echo $row[2].'<br>';
+
+	$tab = array(
+		"title" => $row[0],
+		"seo_title" => $row[1],
+		"url" => $row[2],
+		"author" => $row[3],
+		"date" => $row[4],
+		"category" => $row[5],
+		"locales" => $row[6],
+		"content" => $row[7]
+	);
+
+
+	echo json_encode($tab);
+	echo '<br>';
+	$i++;
+	//if($i == 10){
+		//die();
+	//}
+}
+//echo json_encode($tab);
+//echo '<pre>';
+//var_dump(json_encode($tab));
